a89e8726b8af9dd6293ae262c7381885fb65f46d
Daniel Ethridge <danielethridge@icloud.com>
2019-10-28 23:36:54 -0400

    PWA

--- src/elements/app.mjs
+++ src/elements/app.mjs
@@ -1,3 +1,8 @@
 import buildContextList from "./build-context-list.mjs"
 
 buildContextList()
+
+if ("serviceWorker" in navigator) 
+  window.addEventListener("load", () =>
+    navigator.serviceWorker.register("/service-worker.js")
+  )
