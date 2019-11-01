4e7351de80328e9588823d4f5808de29d5fcc590
Daniel Ethridge <danielethridge@icloud.com>
2019-11-01 12:38:51 -0400

    Allow sort by location and graceful fallback

--- src/elements/build-context-list.mjs
+++ src/elements/build-context-list.mjs
@@ -3,17 +3,23 @@ import editContextCard from "./edit-context-card.mjs"
 
 import { store } from "../store.mjs"
 import { sortContexts } from "../util.mjs"
+import { getLocation } from "../util.mjs"
 
 const buildContextList = () => {
   const container = document.querySelector("body > main")
   container.innerHTML = ""
 
-  const currentLocation = { lat: 0, lon: 0 }
-  const currentTime = new Date()
-
-  sortContexts(store, currentLocation, currentTime)
-    .map(contextCard)
-    .map(card => container.appendChild(card))
+  getLocation()
+    .then(currentLocation =>
+      sortContexts(store, currentLocation, new Date())
+        .map(contextCard)
+        .map(card => container.appendChild(card))
+    ).catch(e => {
+      sortContexts(store, { lat: 0, lon: 0 }, new Date())
+        .map(contextCard)
+        .map(card => container.appendChild(card))
+      alert(`Unable to sort by location - ${e.message}`)
+    })
 
   const fab = document.querySelector("body > footer > button")
 
